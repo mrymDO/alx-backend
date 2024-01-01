06a644be3ea24fd3152d8f4ffebee649f17ac57d
Meryeme <dobli.meryeme@gmail.com>
2024-01-01 05:17:35 -0800

    task0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,5 +6,5 @@ def index_range(page, page_size):
     """return a tuple containing start index and end index"""
     page = max(1, page)
     start_index = (page - 1) * page_size
-    end_index = start_index + page_size - 1
-    return start_index, end_index    
+    end_index = start_index + page_size
+    return start_index, end_index
