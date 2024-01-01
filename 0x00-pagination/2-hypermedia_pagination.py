fc8d8230b321aabda37df31f0296da72008d7220
Meryeme <dobli.meryeme@gmail.com>
2024-01-01 07:59:50 -0800

    task2

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -6,6 +6,14 @@ import math
 from typing import List, Dict
 
 
+def index_range(page, page_size):
+    """return a tuple containing start index and end index"""
+    page = max(1, page)
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
+
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
@@ -26,45 +34,34 @@ class Server:
 
         return self.__dataset
 
-    def indexed_dataset(self) -> Dict[int, List]:
-        """Dataset indexed by sorting position, starting at 0
-        """
-        if self.__indexed_dataset is None:
-            dataset = self.dataset()
-            truncated_dataset = dataset[:1000]
-            self.__indexed_dataset = {
-                i: dataset[i] for i in range(len(dataset))
-            }
-        return self.__indexed_dataset
-
-    def get_hyper(self, index: int = None, page_size: int = 10) -> Dict:
-        """retrieve information about a page"""
-        if self.__indexed_dataset is None:
-            self.indexed_dataset()
-
-        assert isinstance(index, int) and \
-                0 <= index <= len(self.__indexed_dataset)
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """retrieve a page from dataset"""
+        assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
-        start_index = index if index is not None else 0
-        end_index = start_index + page_size
+        start, end = index_range(page, page_size)
+        dataset = self.dataset()
 
-        data_page = [
-                self.__indexed_dataset[i]
-                for i in range(start_index, end_index)
-        ]
+        if start >= len(dataset):
+            return []
 
-        next_index = \
-            end_index if end_index < len(self.__indexed_dataset) else None
+        return dataset[start:min(end, len(dataset))]
+
+    def get_hyper(self, page: int = None, page_size: int = 10) -> Dict:
+        """retrieve information about a page"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        data_page = self.get_page(page, page_size)
+        start_index, end_index = index_range(page, page_size)
+
+        next_index = start_index + page_size if \
+            end_index < len(self.dataset()) else None
 
         return {
-                'page_size': len(data_page),
-                'page': start_index // page_size + 1,
-                'data': data_page,
-                'next_page': next_index // page_size + 1 if \
-                        next_index is not None else None,
-                'prev_page': start_index // page_size if \
-                        start_index > 0 else None,
-                'total_pages': math.ceil(
-                    len(self.__indexed_dataset) / page_size)
+            'page_size': len(data_page),
+            'page': page,
+            'data': data_page,
+            'next_index': next_index,
+            'total_pages': math.ceil(len(self.dataset()) / page_size)
         }
