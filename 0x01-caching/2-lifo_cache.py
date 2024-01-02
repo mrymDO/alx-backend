5822f66cbbf6a1278e9159d638f4414c7b0d904e
Meryeme <dobli.meryeme@gmail.com>
2024-01-02 13:01:43 -0800

    task2

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""LIFO Caching"""
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """caching system"""
+
+    def put(self, key, item):
+        """Add new item"""
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                discarded_key = list(self.cache_data.keys())[-1]
+                del self.cache_data[discarded_key]
+                print(f"DISCARD: {discarded_key}")
+        self.cache_data.update({key: item})
+
+    def get(self, key):
+        """Get item by key"""
+        return self.cache_data.get(key)
