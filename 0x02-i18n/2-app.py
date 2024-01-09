6235d7089abd120bc6cf31d144d7c36b74f5c169
Meryeme <dobli.meryeme@gmail.com>
2024-01-09 13:37:30 -0800

    commit

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -6,16 +6,21 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config:
+    """config falsk babel"""
+
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
+
 
 app.config.from_object(Config)
 
 
 @babel.localeselector
 def get_locale():
+    """Gets the best match from supported languages."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
