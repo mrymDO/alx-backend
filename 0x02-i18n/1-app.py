53d9f1af82521267d1c070f60a2d95ddea40fc0e
Meryeme <dobli.meryeme@gmail.com>
2024-01-09 21:12:23 +0100

    add index.html

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    """config falsk babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def index():
+    """index"""
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
