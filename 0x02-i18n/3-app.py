7f6cdf6bb7814120a035818e52a52d0ed3cd8a84
Meryeme <dobli.meryeme@gmail.com>
2024-01-09 22:36:01 +0100

    task 3

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -21,15 +21,15 @@ app.config.from_object(Config)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """Gets the best match from supported languages."""
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route('/')
+@app.route("/")
 def index():
     """index"""
-    return render_template('3-index.html', title=_("Welcome to Holberton"), header=_("Hello world!"))
+    return render_template("3-index.html", home_title=_('Welcome to Holberton'), home_header=_('Hello world!'))
 
 
 if __name__ == '__main__':
