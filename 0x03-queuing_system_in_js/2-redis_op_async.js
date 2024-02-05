37b1a6203833604bf75345333d80674c8c8fba96
Meryeme <dobli.meryeme@gmail.com>
2024-02-05 12:51:57 -0800

    task3

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,24 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+	console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+	console.error(`Redis client not connected to the server: ${err}`);
+});
+
+function setNewSchool(schoolName, value) {
+	client.set(schoolName, value, redis.print);
+}
+
+async function displaySchoolValue(schoolName) {
+	const resp = await client.get(schoolName);
+	console.log(resp);
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
