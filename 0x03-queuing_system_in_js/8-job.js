7fe752f5f27363e02adc9edca1bc714b4a14d86c
Meryeme <dobli.meryeme@gmail.com>
2024-02-06 02:56:11 -0800

    task10

---
+++ 0x03-queuing_system_in_js/8-job.js
@@ -0,0 +1,24 @@
+function createPushNotificationsJobs(jobs, queue) {
+	if (!Array.isArray(jobs)) {
+		throw new Error('Jobs is not an array');
+	}
+
+	jobs.forEach((jobData) => {
+		const job = queue.create('push_notification_code_3', jobData);
+		job.save((err) => {
+			if (!err) {
+				console.log(`Notification job created: ${job.id}`);
+			}
+		});
+
+		job.on('failed', (err) => {
+			console.log(`Notification job ${job.id} failed: ${err}`);
+		});
+
+		job.on('progress', (progress, data) => {
+			console.log(`Notification job ${job.id} ${progress}% complete`);
+		});
+	});
+}
+
+export default createPushNotificationsJobs;
